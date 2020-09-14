939d7d3e720bcfb7214844de31e928875d702bfc
Thomas Burgess <thomasburgess2000@gmail.com>
2020-09-14 00:43:49 -0500

    updated

--- typer.py
+++ typer.py
@@ -5,8 +5,72 @@ import busio
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1305
+#import keyboard
+from pynput import keyboard
 
 oled_reset = digitalio.DigitalInOut(D4)
 i2c = busio.I2C(SCL, SDA)
 
 disp = adafruit_ssd1305.SSD1305_I2C(128, 32, i2c, reset=oled_reset)
+
+# Clear display.
+disp.fill(0)
+disp.show()
+ 
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+ 
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+ 
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+ 
+ 
+# Load default font.
+font = ImageFont.load_default()
+ 
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+#font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
+
+string = ""
+
+#FUNCTIONS
+def on_press(key):
+    try:
+        string = string + '{0}'.format(key.char))
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw.text((x, top+0), string, font=font, fill=255)
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+while True:
+    disp.image(image)
+        disp.show()
+        time.sleep(.1)
+        
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
