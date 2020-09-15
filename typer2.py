b749ee82c69c884c27e39cd4d9fd2f917f266984
Thomas Burgess <thomasburgess2000@gmail.com>
2020-09-14 20:05:53 -0500

    updated

--- typer2.py
+++ typer2.py
@@ -53,6 +53,10 @@ while True:
     #print ("key:", keypress)
     outputstring = outputstring + chr(keypress)
     draw.rectangle((0,0,width,height),outline=0,fill=black)
-    draw.text((x, top+0), outputstring, font=font, fill=white)
+    if len(outputstring) < 21:
+        draw.text((x, top+0), outputstring, font=font, fill=white)
+    elif len(outputstring) < 41:
+        draw.text((x, top+0), outputstring[:20], font=font, fill=white)
+        draw.text((x, top+8), outputstring[20:], font=font, fill=white)
     disp.image(image)
     disp.show()
