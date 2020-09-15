9e994eae0d6858aca0aefcbd45f5e912cdcebdaf
Thomas Burgess <thomasburgess2000@gmail.com>
2020-09-14 20:34:42 -0500

    updated

--- typer2.py
+++ typer2.py
@@ -71,5 +71,10 @@ while True:
         draw.text((x, top+8), outputstring[21:41], font=font, fill=white)
         draw.text((x, top+16), outputstring[41:61], font=font, fill=white)
         draw.text((x, top+25), outputstring[61:], font=font, fill=white)
+    elif len(outputstring) >= 81:
+        draw.text((x, top+0), outputstring[-80:-60], font=font, fill=white)
+        draw.text((x, top+8), outputstring[-60:-40], font=font, fill=white)
+        draw.text((x, top+16), outputstring[-40:-20], font=font, fill=white)
+        draw.text((x, top+25), outputstring[-20:], font=font, fill=white)
     disp.image(image)
     disp.show()
