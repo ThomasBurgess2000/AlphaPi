023d1cd1e5e56bb67c1989b5fe1e7a7cd857ee25
Thomas Burgess <thomasburgess2000@gmail.com>
2020-09-14 20:27:56 -0500

    updated

--- typer2.py
+++ typer2.py
@@ -70,6 +70,6 @@ while True:
         draw.text((x, top+0), outputstring[:21], font=font, fill=white)
         draw.text((x, top+8), outputstring[21:41], font=font, fill=white)
         draw.text((x, top+16), outputstring[41:61], font=font, fill=white)
-        draw.text((x, top+16), outputstring[61:], font=font, fill=white)
+        draw.text((x, top+25), outputstring[61:], font=font, fill=white)
     disp.image(image)
     disp.show()
